67c176f02bd6bad76c3f8690811b6f1482a43eb1
IrvingGlt <41974385+Krpeta21@users.noreply.github.com>
2023-01-15 12:34:13 -0600

    crud-java

---
+++ src/main/java/com/krpeta/CRUD/dao/UsuarioDao.java
@@ -0,0 +1,16 @@
+package com.krpeta.CRUD.dao;
+
+import com.krpeta.CRUD.models.Usuario;
+
+import java.util.List;
+
+public interface UsuarioDao {
+
+    List<Usuario> getUsuarios();
+
+    void eliminar(Long id);
+
+    void registrar(Usuario usuario);
+
+    Usuario obtenerUsuarioPorCredenciales(Usuario usuario);
+}
